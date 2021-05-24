f60ba771a675f058448dd254dbba88cfc9c7e873
Chinmay <Chinmay8116@gmail.com>
2021-05-24 06:37:07 +0530

    commit balanced brackets

---
+++ Balanced Brackets/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Balanced_Brackets
+{
+    public static class AppHelper
+    {
+        public static Boolean isMatching(Char a, Char b)
+        {
+            return ((a == '(' && b == ')') || (a == '[' && b == ']') || (a == '{' && b == '}'));
+        }
+
+        public static Boolean IsBalanced(String str)
+        {
+            Stack<Char> stack = new Stack<Char>();
+            for (Int32 i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '(' || str[i] == '{' || str[i] == '[')
+                {
+                    stack.Push(str[i]);
+                }
+                else if (str[i] == ')' || str[i] == ']' || str[i] == '}')
+                {
+                    if (stack.Count == 0)
+                        return false;
+                    else if (isMatching(stack.Peek(), str[i]))
+                        stack.Pop();
+                    else
+                        return false;
+                }
+            }
+            return stack.Count == 0 ? true : false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = "([])(){}(())()()";
+            Console.WriteLine(AppHelper.IsBalanced(str));
+            Console.ReadLine();
+        }
+    }
+}
