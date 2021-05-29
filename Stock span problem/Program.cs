f97e2706ae2d01bcc8dc0dfefb43341d9560d459
Chinmay <Chinmay8116@gmail.com>
2021-05-29 12:03:31 +0530

    commit-stock span problem

---
+++ Stock span problem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stock_span_problem
+{
+    public static class AppHelper
+    {
+        public static void StockSpan(Int32 [] arr,Int32 n)
+        {
+            Stack<Int32> stack = new Stack<Int32>();
+            stack.Push(0);
+            Console.Write("1 ");
+            for(Int32 i=1;i<n;i++)
+            {
+                while(stack.Count !=0 && arr[stack.Peek()]<=arr[i])
+                {
+                    stack.Pop();
+                }
+                Int32 span = stack.Count == 0 ? i + 1 : i - stack.Peek();
+                Console.Write(span + " ");
+                stack.Push(i);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr={18,12,13,14,11,16};
+            AppHelper.StockSpan(arr, arr.Length);
+            Console.ReadLine();
+        }
+    }
+}
