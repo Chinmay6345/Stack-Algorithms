f9238961da95f81c9ee925f76398141ed1e33a44
Chinmay <Chinmay8116@gmail.com>
2021-06-02 08:52:17 +0530

    added hierarchy

---
+++ 844 Backspace String Compare/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _844_Backspace_String_Compare
+{
+    public static class AppHelper
+    {
+        public static bool AreEqual(String s, String t)
+        {
+            Stack<Char> s1 = new Stack<Char>();
+            Stack<Char> s2 = new Stack<Char>();
+
+            foreach (Char c in s)
+            {
+                if (c != '#')
+                    s1.Push(c);
+                else if (s1.Count != 0)
+                    s1.Pop();
+            }
+
+            foreach (Char c in t)
+            {
+                if (c != '#')
+                    s2.Push(c);
+                else if (s2.Count != 0)
+                    s2.Pop();
+            }
+            return s1.SequenceEqual(s2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isvalid=  AppHelper.AreEqual("a##c","#a#c");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Maximal Rectangle/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Maximal_Rectangle
+{
+    public static class AppHelper
+    {
+        public static int largestHist(List<int> buildings)
+        {
+            Int32 maxArea = 0;
+            Stack<Int32> stack = new Stack<Int32>();
+            stack.e
+            buildings.Add(0);
+            List<Int32> ext = new List<Int32>(buildings);
+            for (Int32 i = 0; i < ext.Count; i++)
+            {
+                Int32 height = ext[i];
+                while (stack.Count != 0 && ext[stack.Peek()] >= height)
+                {
+                    Int32 pHeight = ext[stack.Pop()];
+                    Int32 width = stack.Count == 0 ? i : i - stack.Peek() - 1;
+                    maxArea = Math.Max(maxArea, (width * pHeight));
+                }
+                stack.Push(i);
+            }
+            return maxArea;
+        }
+
+        public static int maxRectangle(int R, int C, int [][] mat)
+        {
+            int res = largestHist(R, C, mat[0]);
+
+            for (int i = 1; i < R; i++)
+            {
+
+                for (int j = 0; j < C; j++)
+
+                    if (mat[i][j] == 1)
+                        mat[i][j] += mat[i - 1][j];
+
+                res = Math.Max(res, largestHist(mat[i]));
+            }
+            return res;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            }
+        }
+    }
