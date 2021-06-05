fbe94b49ee8298a829041d72fa04afbbcc71466a
Chinmay <Chinmay8116@gmail.com>
2021-06-05 16:55:47 +0530

    commit Generate_numbers_with_given_digits

---
+++ Generate numbers with given digits/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generate_numbers_with_given_digits
+{
+    public static class AppHelper
+    {
+        public static void GenerateNumbers(Int32 n)
+        {
+            Queue<String> q = new Queue<String>();
+            q.Enqueue("5");
+            q.Enqueue("6");
+            for(Int32 i=0;i<n;i++)
+            {
+                String curr = q.Peek();
+                Console.Write(q.Dequeue() + " ");
+                q.Enqueue(curr + "5");
+                q.Enqueue(curr + "6");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppHelper.GenerateNumbers(5);
+            Console.ReadLine();
+        }
+    }
+}
